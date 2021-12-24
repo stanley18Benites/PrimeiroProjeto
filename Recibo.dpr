d3e087064b27101f3e59e7c95e076bb4b0a421cb
Stanley <stanley_antonio@outlook.com>
2021-12-24 11:26:05 -0400

    Primeiro commit

---
+++ Recibo.dpr
@@ -0,0 +1,14 @@
+program Recibo;
+
+uses
+  Vcl.Forms,
+  Unit1 in 'Unit1.pas' {frmMenu};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TfrmMenu, frmMenu);
+  Application.Run;
+end.
