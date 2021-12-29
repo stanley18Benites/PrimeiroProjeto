5a719c8fadd05b18b290813826f76a197a8f3602
stanley <stanley_antonio@outlook.com>
2021-12-29 11:35:29 -0400

    Alteracao layout

--- uCadastro.pas
+++ uCadastro.pas
@@ -120,8 +120,8 @@ begin
     if MaskEdit3.Text <> EmptyStr then
       frmRecibo.lbInscEstadual.Caption := MaskEdit3.Text;
      frmRecibo.edtFone.Caption := str;
-     frmRecibo.lbCidade.Caption := edtCidadeEmpresa.Text + '-';
-     frmRecibo.lbEstado.Caption := edtEstado.Text + ' ,';
+     frmRecibo.lbCidade.Caption := edtCidadeEmpresa.Text + ' - ';
+     frmRecibo.lbEstado.Caption := edtEstado.Text + ',';
      frmRecibo.lbNomEmpresa.Caption := edtNomeEmp.Text;
      frmRecibo.RLReport1.Preview;
      boxTelOpcional.ItemIndex := 0;
